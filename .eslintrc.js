8fcc18ee1c5e953305c7cc9d6f3318eb85f3b395
Andrey Kozhuhov <andrpre96@gmail.com>
2024-07-03 22:45:17 +0300

    message

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,5 +32,6 @@ module.exports = {
 		'react/jsx-uses-react': 'off',
 		'react/react-in-jsx-scope': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		"endOfLine": "auto"
 	},
 };
